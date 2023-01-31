488624e734542aa320a13a4925c1afe0fcc63e4a
Mab <mabchhan@gmail.com>
2023-01-30 18:41:49 -0800

    stash

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const { Project, User } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('homepage', { 
-        title: 'Homepage', 
-        message: 'Welcome to the homepage' 
-    });
+router.get('/', async (req, res) => {
+  try {
+    const dbProject = await User.findAll();
+    console.log(dbProject);
+    // const projects = dbProject.map((project) => {
+    //   project.get({ plain: true });
+    // });
+    res.json(dbProjects);
+    // res.render('homepage', {
+    //   projects,
+    //   //   //loggedIn: req.session.loggedIn,
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+  //   if (req.session.loggedIn) {
+  //     res.redirect('/');
+  //     return;
+  //   }
+
+  res.render('login');
 });
 
 module.exports = router;
--- seeds/userData.js
+++ seeds/userData.js
@@ -1,33 +1,33 @@
 const { User } = require('../models');
 
 const userData = [
-    {
-        name: 'User Name',
-        starting_date: 'Date',
-        deadline: 'Date',
-    },
-    {
-        name: 'User Name',
-        starting_date: 'Date',
-        deadline: 'Date',
-    },
-    {
-        name: 'User Name',
-        starting_date: 'Date',
-        deadline: 'Date',
-    },
-    {
-        name: 'User Name',
-        starting_date: 'Date',
-        deadline: 'Date',
-    },
-    {
-        name: 'User Name',
-        starting_date: 'Date',
-        deadline: 'Date',
-    },
+  {
+    username: 'User Name1',
+    email: 'user1@gmail.com',
+    password: '123456',
+  },
+  {
+    username: 'User Name2',
+    email: 'user2@gmail.com',
+    password: '123456',
+  },
+  {
+    username: 'User Name3',
+    email: 'user3@gmail.com',
+    password: '123456',
+  },
+  {
+    username: 'User Name4',
+    email: 'user4@gmail.com',
+    password: '123456',
+  },
+  {
+    username: 'User Name5',
+    email: 'user5@gmail.com',
+    password: '123456',
+  },
 ];
 
-const seedUsers = () => User.bulkcreate(userData);
+const seedUsers = () => User.bulkCreate(userData);
 
 module.exports = seedUsers;
